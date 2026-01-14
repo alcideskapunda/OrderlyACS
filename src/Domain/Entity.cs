a2ff30c1cc36d2bb28e6ba3990942265d7b60e4a
alcideskapunda <alcideskapunda22@gmail.com>
2026-01-14 10:52:34 -0300

    feat: entity category

---
+++ src/Domain/Entity.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OrderlyACS.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    // public string Name { get; set; }
+    public string CreateBy { get; set; }
+    public DateTime CreateOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
