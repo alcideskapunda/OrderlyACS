68a37f01072d4e3e9bbb0614607129eee25ce71a
alcideskapunda <alcideskapunda22@gmail.com>
2026-01-14 12:58:35 -0300

    feat: add C# extension method

---
+++ src/Endpoints/ProblemDetailsExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using Flunt.Notifications;
+
+namespace OrderlyACS.Endpoints;
+
+public static class ProblemDetailsExtensions
+{
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notifications)
+    {
+        return notifications
+            .GroupBy(g => g.Key)
+            .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
+    }
+}
