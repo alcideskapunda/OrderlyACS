63545c3f604653ef5cd00e24e6b981d74a66d2b5
alcideskapunda <alcideskapunda22@gmail.com>
2026-01-14 13:05:13 -0300

    feat: add domain validation to Category entity

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,9 +1,43 @@
 using System;
+using Flunt.Validations;
 
 namespace OrderlyACS.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; }
-    public bool Active { get; set; } = true;
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
+
+    public Category(string name, string createBy, string editedBy)
+    {
+        Name = name;
+        Active = true;
+        CreateBy = createBy;
+        CreateOn = DateTime.Now;
+        EditedBy = editedBy;
+        EditedOn = DateTime.Now;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        Clear();
+
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(Name, "Name")
+            .IsGreaterOrEqualsThan(Name, 3, "Name")
+            .IsNotNullOrEmpty(CreateBy, "CreateBy")
+            .IsNotNullOrEmpty(EditedBy, "EditedBy");
+
+        AddNotifications(contract);
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validate();
+    }
 }
