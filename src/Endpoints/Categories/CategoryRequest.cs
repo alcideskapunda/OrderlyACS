aa1a3d659194c99717e2ce97eec813befcafa487
alcideskapunda <alcideskapunda22@gmail.com>
2026-01-14 10:52:08 -0300

    feat: add request and response DTOs

---
+++ src/Endpoints/Categories/CategoryRequest.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace OrderlyACS.Endpoints.Categories;
+
+public class CategoryRequest
+{
+    public string Name { get; set; }
+    public bool Active { get; set; }
+}
---
+++ src/Endpoints/Categories/CategoryResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace OrderlyACS.Endpoints.Categories;
+
+public class CategoryResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool Active { get; set; }
+}
